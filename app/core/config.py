7eea33034bcd00abab04c01e45b9388a335b22b7
anxious1 <54373328+anxious1@users.noreply.github.com>
2025-06-05 11:07:25 +0300

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -4,11 +4,11 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str = "sqlite:///./app.db"
-    JWT_SECRET_KEY: str = "your_super_secret_key"  # Замените!
+    JWT_SECRET_KEY: str = "your_super_secret_key"
     JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
     class Config:
-        env_file = ".env"  # если используете .env
+        env_file = ".env"
 
 settings = Settings()
